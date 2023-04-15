d5c46d9273483156bb2646a1f4b51cc0737244d4
AnumtaKhan <anumtak_8@hotmail.com>
2023-04-15 14:57:09 -0400

    pushing final update

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,12 +1,117 @@
-// Store our API endpoing as queryURL
+// Store API endpoing as queryURL
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL
-d3.json(url).then(function(data){
+// Perform a GET request to the url
+d3.json(url).then(function(data) {
+    // Send object to createFeatures function
     createFeatures(data.features);
   });
 
 // Create a function named `createFeatures` that takes `earthquakeData` and `platesData` as an argument.
-function createFeatures(earthquakeData, platesData){
+function createFeatures(earthquakeData) {
+
+    // Create popups that provide additional information about the earthquake when its marker is clicked.
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Where: ${feature.properties.place}</h3><hr><p>Time: ${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}<p>Depth: ${feature.geometry.coordinates[2]}km</p>`);
+    }
+
+   // Create a GeoJSON layer on the earthquakeData object that has the features array
+   function createCircleMarker(feature, latlong) {
+      let options = {
+       radius:feature.properties.mag*5,
+       fillColor: chooseColor(feature.properties.mag),
+       color: chooseColor(feature.properties.mag),
+       weight: 1,
+       opacity: 0.8,
+       fillOpacity: 0.35
+      } 
+      return L.circleMarker(latlong,options);
+   }
+
+   // Create a variable for earthquakes to have the latlong, popup feature, and the radious, colour, weight, and opacity of each circle
+   let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: createCircleMarker
+   });
+
+   // Send the earthquakes layer to the createMap function
+   createMap(earthquakes);
+}
+
+  // Create Data markers that reflect the magnitude of the earthquake by their size and the depth of the earthquake by color.
+  function chooseColor(mag) {
+      switch(true) {
+          case(1.0 <= mag && mag <= 2.5):
+              return "#0071BC"; // Strong blue
+          case (2.5 <= mag && mag <=4.0):
+              return "#35BC00";
+          case (4.0 <= mag && mag <=5.5):
+              return "#BCBC00";
+          case (5.5 <= mag && mag <= 8.0):
+              return "#BC3500";
+          case (8.0 <= mag && mag <=20.0):
+              return "#BC0000";
+          default:
+              return "#E2FFAE";
+      }
+  }
+
+  // Create a legend that provides context for the map data
+  let legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function() {
+      let div = L.DomUtil.create('div', 'info legend');
+      let grades = [1.0, 2.5, 4.0, 5.5, 8.0];
+      let labels = [];
+      let legendInfo = "<h4>Magnitude</h4>";
+
+      div.innerHTML = legendInfo
+
+      for (let i = 0; i < grades.length; i++) {
+            labels.push('<ul style="background-color:' + chooseColor(grades[i] + 1) + '"> <span>' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '' : '+') + '</span></ul>');
+      }
+
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  
+      return div;
+  };
 
 
+  // Create map
+  function createMap(earthquakes) {
+
+    // Define layers
+    let googleSatellitemap = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+      maxZoom: 10,
+      subdomains:['mt0','mt1','mt2','mt3'],
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    let streetlayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Define a baseMaps object to hold our base layers
+    let baseMaps = {
+      "Google Satellite Map": googleSatellitemap,
+      "Street Layer": streetlayer
+    };
+
+    // Create overlay object to hold our overlay layer
+    let overlayMaps = {
+      "Earthquakes": earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    let myMap = L.map("map", {
+      center: [39.8282, -98.5795],
+      zoom: 5,
+      layers: [streetlayer, googleSatellitemap, earthquakes]
+    });
+
+    // Add layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+    legend.addTo(myMap);
+  }
